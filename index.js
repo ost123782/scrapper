67ed847799cc76b67697dbe9c0a8ed3a81dd12c8
ost123 <83078347+ost123782@users.noreply.github.com>
2024-04-03 16:50:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 
 async function scrapSite () {
 
-    const response = await axios.get('http://lyceum-lvivskyi.lviv.sch.in.ua/')
+    const response = await axios.get('PASTE YUT LINK HERE')
 
     fs.writeFile('index.html', response.data, (err) => {
         if (err) throw err;
